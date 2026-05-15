3dea99412d204d29dad5dfd6c75482157d78c82e
Son Do <dhson8396@gmail.com>
2026-05-15 11:03:13 +0700

    chore: remove app config and update meta information in nuxt.config.ts

--- app/app.config.ts
+++
@@ -1,4 +0,0 @@
-export default defineAppConfig({
-  title: "Nuxt Boilerplate",
-  description: "Nuxt Boilerplate - A starting point for Nuxt projects",
-});
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
 
+const siteMeta = {
+  title: "Nuxt Boilerplate",
+  description: "Nuxt Boilerplate - A starting point for Nuxt projects",
+};
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-10-04",
@@ -7,7 +12,17 @@ export default defineNuxtConfig({
   // debug: true, // Uncomment this line to enable debug mode
   devServer: { https: true },
   // ssr: false, // Uncomment this line to disable server-side rendering
-  app: { rootTag: "app" },
+  app: {
+    rootTag: "app",
+    head: {
+      title: siteMeta.title,
+      meta: [
+        { name: "description", content: siteMeta.description },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
   css: ["~/assets/css/tailwind.css"],
   modules: ["@nuxt/eslint", "@vueuse/nuxt", "@pinia/nuxt", "nuxt-svgo"],
   vite: {
